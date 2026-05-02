55593e3fdf65da8b57090696ea7870b02a386026
Rohit Pandey <roni914024@gmail.com>
2026-05-02 18:16:05 +0530

    Added weather API integration

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -5,6 +5,24 @@ import { useState } from 'react';
 
 export default function SearchBox(){
     let[city,setCity]=useState("");
+    const API_URL="https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY="63db83900a890023decd9c8a9799fc82";
+
+        let getWeatherInfo=async ()=>{
+            let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+            let jsonResponse=await response.json();
+        let result={
+            temp:jsonResponse.main.temp,
+            tempMin:jsonResponse.main.temp_min,
+            tempMax:jsonResponse.main.temp_max,
+            humidity:jsonResponse.main.humidity,
+            feelsLike:jsonResponse.main.feels_like,
+            weather:jsonResponse.weather[0].description,
+
+        } ;
+        console.log(result);
+    };
+
 
     let handleChange=(evt)=>{
         setCity(evt.target.value);
@@ -13,6 +31,8 @@ export default function SearchBox(){
         evt.preventDefault();
         console.log(city);
         setCity("");
+        getWeatherInfo();
+
     }
 
     return(
