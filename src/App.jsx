4e0cd8987083253583d06bbc298c764d30597d7b
Rohit Pandey <roni914024@gmail.com>
2026-05-02 16:52:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import SearchBox from "./SearchBox";
+
+function App() {
+  let handleClick=()=>{
+    console.log("button was clicked");
+  }
+
+  return (
+    <>
+    <SearchBox/>
+    
+
+    </>
+  )
+}
+
+export default App
